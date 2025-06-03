c34815703b622b9dccf4853f7dd4aa556852cfd0
Liana Conde Outeiral <144838284+LianaConde@users.noreply.github.com>
2025-06-02 22:12:00 -0300

    Update SELECT from WORLD Tutorial.sql

--- SELECT from WORLD Tutorial.sql
+++ SELECT from WORLD Tutorial.sql
@@ -10,3 +10,15 @@ WHERE population >= 200000000;
 SELECT name, population / 1000000 AS population_in_millions
 FROM world
 WHERE continent = 'South America';
+
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy');
+
+SELECT name
+FROM world
+WHERE name LIKE '%United%';
+
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 OR population > 250000000;
